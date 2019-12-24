18abc77b832041da170b931a7b26834a5216d444
carlosgat <carlosambrona.t@gmail.com>
2019-12-24 15:11:37 +0100

    tobn

--- app/src/main/java/com/example/ahorcado/JuegoActivity.java
+++ app/src/main/java/com/example/ahorcado/JuegoActivity.java
@@ -10,5 +10,6 @@ public class JuegoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juego);
+
     }
 }
--- app/src/main/java/com/example/ahorcado/NombreActivity.java
+++ app/src/main/java/com/example/ahorcado/NombreActivity.java
@@ -2,13 +2,25 @@ package com.example.ahorcado;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NombreActivity extends AppCompatActivity {
-
+    private Button bt_ok;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nombre);
+
+        bt_ok = findViewById(R.id.bt_ok);
+        bt_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NombreActivity.this, JuegoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

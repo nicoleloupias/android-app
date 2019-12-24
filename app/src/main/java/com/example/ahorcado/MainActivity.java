5fb7a1030377aeacf56fce602036badbe62a93b5
carlosgat <carlosambrona.t@gmail.com>
2019-12-24 15:03:30 +0100

    intent

--- app/src/main/java/com/example/ahorcado/MainActivity.java
+++ app/src/main/java/com/example/ahorcado/MainActivity.java
@@ -2,20 +2,31 @@ package com.example.ahorcado;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tv_tituloJuego;
+
     private Button bt_jugar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bt_jugar = findViewById(R.id.bt_jugar);
 
+        bt_jugar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NombreActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }

7f30e50cc9c40ab1d72c67a8f4d2423c8da7452d
Nicole Loupias <loupiasnicole@gmail.com>
2019-12-24 12:05:30 +0100

    first commit

---
+++ app/src/main/java/com/example/ahorcado/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.ahorcado;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv_tituloJuego;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv_tituloJuego = findViewById(R.id.tv_tituloJuego);
+
+        Typeface face=Typeface.createFromAsset(getAssets(),"fonts/pigment-demo.regular.otf");
+        tv_tituloJuego.setTypeface(face);
+    }
+}

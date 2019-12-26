bd3e73b7b796cfbf52de0e920a794f1134fbe5c4
Nicole Loupias <loupiasnicole@gmail.com>
2019-12-26 22:12:29 +0100

    dificultadElegida

--- app/src/main/java/com/example/ahorcado/DificultadActivity.java
+++ app/src/main/java/com/example/ahorcado/DificultadActivity.java
@@ -2,13 +2,44 @@ package com.example.ahorcado;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DificultadActivity extends AppCompatActivity {
+
+    private Button bt_facil;
+    private Button bt_normal;
+    private Button bt_dificil;
+    private int dificultadElegida = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dificultad);
+
+        bt_facil = findViewById(R.id.bt_facil);
+        bt_normal = findViewById(R.id.bt_normal);
+        bt_dificil = findViewById(R.id.bt_dificil);
+    }
+
+    public void dificultadClickeada(View v){
+
+        switch (v.getId()){
+            case R.id.bt_facil:
+                dificultadElegida = 1;
+                break;
+            case R.id.bt_normal:
+                dificultadElegida = 2;
+                break;
+            case R.id.bt_dificil:
+                dificultadElegida = 3;
+                break;
+        }
+
+        Intent i = new Intent(DificultadActivity.this, JuegoActivity.class);
+        i.putExtra("DIFICULTAD", dificultadElegida);
+        startActivity(i);
     }
 }
